5b2d5de45a862e388f541bab4e070158d06451c3
Rayane Leandro de Lima Nogueira <rayaneleandrodelima2016@gmail.com>
2025-12-05 18:50:45 -0300

    Enviando Menu.ts para o repositório

---
+++ Menu.js
@@ -0,0 +1,72 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = main;
+exports.sobre = sobre;
+function main() {
+    let opcao;
+    while (true) {
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("                BANCO DO BRAZIL COM Z                ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("            1 - Criar Conta                          ");
+        console.log("            2 - Listar todas as Contas               ");
+        console.log("            3 - Buscar Conta por Numero              ");
+        console.log("            4 - Atualizar Dados da Conta             ");
+        console.log("            5 - Apagar Conta                         ");
+        console.log("            6 - Sacar                                ");
+        console.log("            7 - Depositar                            ");
+        console.log("            8 - Transferir valores entre Contas      ");
+        console.log("            9 - Sair                                 ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("Entre com a opção desejada: ");
+        opcao = readlineSync.questionInt("");
+        if (opcao == 9) {
+            console.log("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+            sobre();
+            Process.exit(0);
+        }
+        switch (opcao) {
+            case 1:
+                console.log("\n\nCriar Conta\n\n");
+                break;
+            case 2:
+                console.log("\n\nLista todas as contas\n\n");
+                break;
+            case 3:
+                console.log("\n\nConsultar dados da Conta - por número\n\n");
+                break;
+            case 4:
+                console.log("\n\nAtualizar dados da Conta\n\n");
+                break;
+            case 5:
+                console.log("\n\nApagar uma Conta\n\n");
+                break;
+            case 6:
+                console.log("\n\nSaque\n\n");
+                break;
+            case 7:
+                console.log("\n\nDepósito\n\n");
+                break;
+            case 8:
+                console.log("\n\nTransferência entre Contas\n\n");
+                break;
+            default:
+                console.log("\nOpção Inválida!\n");
+        }
+    }
+}
+/* Função com os dados da pessoa desenvolvedora */
+function sobre() {
+    console.log("\n******************************************************");
+    console.log("Projeto Desenvolvido por: ");
+    console.log("Generation Brasil - generation@generation.org");
+    console.log("gitnub.com/conteudoGeneration");
+    console.log("********************************************************");
+}
+main();
+//# sourceMappingURL=Menu.js.map
