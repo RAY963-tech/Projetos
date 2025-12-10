7a5ad6af46338b7d8e452e610b96d27d3e98cda5
Rayane Leandro de Lima Nogueira <rayaneleandrodelima2016@gmail.com>
2025-12-10 11:19:13 -0300

    Corrigindo pasta repository e adicionando ContaRepository.ts correto

--- src/repository/ContaRepository.ts
+++
@@ -1,9 +0,0 @@
-import { Conta } from "../model/Conta";
-
-export interface ContaRepository {
-    procurarPorNumero(numero: number): Conta | undefined;
-    listarTodas(): void;
-    cadastrar(conta: Conta): void;
-    atualizar(conta: Conta): void;
-    deletar(numero: number): void;
-}
